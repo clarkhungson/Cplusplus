6329ba39342339e544a9bcc9807af8125e607cb9
thson <thson@humaxdigital.com>
2017-03-07 10:15:34 +0700

    blabla

---
+++ demo.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+int main(){
+	int a;
+	a = 10;
+	cout << a << endl;
+//	char * st;
+	//delete st;
+	return 0;
+}
